9846c733ce432188b5f095573b99e7b302f0bcf4
krishivpiduri2010 <59217701+krishivpiduri2010@users.noreply.github.com>
2021-10-07 19:42:53 -0700

    Ready for deployments

--- joke/views.py
+++ joke/views.py
@@ -1,5 +1,11 @@
+from .models import *
 from django.shortcuts import render
 
+
 # Create your views here.
-def home(request,joke_num):
-    return render(request,'index.html',{'url':str(joke_num+1)})
+def home(request, joke_num):
+    jokes = Joke.objects.all()
+    next = max(list(map(lambda x: x.id, list(jokes))))
+    print(next)
+    return render(request, 'index.html',
+                  {'url': str(joke_num + 1 if next > joke_num else ''), 'joke': Joke.objects.get(id=joke_num)})
